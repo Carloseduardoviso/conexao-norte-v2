d8be10431cc5e482fc12ea419023ef365896baee
Odnanzd <103295377+Odnanzd@users.noreply.github.com>
2022-04-14 16:46:21 -0400

    Dados dos serviços atualizados.

--- js/graficoServicos.js
+++ js/graficoServicos.js
@@ -4,12 +4,12 @@ google.charts.setOnLoadCallback(desenhar);
 function desenhar(){
     var dados = google.visualization.arrayToDataTable([
         ['Servicos', 'Votos', {role: 'style'}, {role: 'annotation'}],
-        ['Jogar', 28, '#00BFFF', '58,3%'],
-        ['Assistir serviços de streamming', 39, '#00BFFF', '81,3%'],
-        ['Estudar', 36, '#00BFFF', '75%'],
-        ['Trabalhar', 19, '#00BFFF', '39,6%'],
-        ['Escutar músicas', 28, '#00BFFF', '58,3%'],
-        ['Fazer Downloads diversos', 30, '#00BFFF', '62,5%'],
+        ['Jogar', 29, '#00BFFF', '58,3%'],
+        ['Assistir serviços de streamming', 40, '#00BFFF', '81,3%'],
+        ['Estudar', 37, '#00BFFF', '75%'],
+        ['Trabalhar', 20, '#00BFFF', '39,6%'],
+        ['Escutar músicas', 29, '#00BFFF', '58,3%'],
+        ['Fazer Downloads diversos', 31, '#00BFFF', '62,5%'],
         ['Outros', 6, '#00BFFF', '10,5%']
     ]);
     var opcoes = {
