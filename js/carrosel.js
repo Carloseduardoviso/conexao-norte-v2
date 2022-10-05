cca66c1c1bf4e911776374b1ff6712d77ab6a4fc
euLucs <ls93894@gmail.com>
2022-10-05 05:39:39 -0400

    conexao norte teste

--- js/carrosel.js
+++ js/carrosel.js
@@ -1,33 +1,8 @@
-const slides = document.querySelectorAll(".item-carrosel")
-const nextButton = document.querySelector(".arrow-right")
-const prevButton = document.querySelector(".arrow-left")
-
-let currentSlideImage = 0
-
-function trocaCurrentItem(currentSlideImage) {
-  slides.forEach(slide => {
-    slide.classList.remove("current-item");
+$(document).ready(function () {
+  $('.carrossel').slick({
+    infinete: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    dots: true
   })
-
-  slides[currentSlideImage].classList.add("current-item")
-}
-
-nextButton.addEventListener("click", () => {
-  if (currentSlideImage === slides.length - 1) {
-    currentSlideImage = 0
-  } else {
-    currentSlideImage++
-  } 
-
-  trocaCurrentItem(currentSlideImage)
-})
-
-prevButton.addEventListener("click", () => {
-  if (currentSlideImage === 0) {
-    currentSlideImage = slides.length - 1
-  } else {
-    currentSlideImage--
-  }
-
-  trocaCurrentItem(currentSlideImage)
-})
+});
