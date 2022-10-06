f24614248a568d5ecf481acb263fdcf5133d3fe5
euLucs <ls93894@gmail.com>
2022-10-06 16:20:10 -0400

    E o ultimo, eu juro

--- js/carrosel.js
+++ js/carrosel.js
@@ -3,6 +3,34 @@ $(document).ready(function () {
     infinete: true,
     slidesToShow: 3,
     slidesToScroll: 3,
-    dots: true
-  })
+    dots: true,
+    autoplay: true,
+    autoplayspeed: 2000,
+
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 630,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+    ],
+  });
 });
