dd9f3e0ad074f3b980a566e74f03d33382d641d0
euLucs <ls93894@gmail.com>
2022-10-06 13:15:28 -0400

    Ultima versão carrossel

--- js/carrosel.js
+++ js/carrosel.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
   $('.carrossel').slick({
     infinete: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 3,
+    slidesToScroll: 3,
     dots: true
   })
 });
